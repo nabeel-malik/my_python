a51633cd6f2944ee1cff343faa9d7f638458a629
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2019-05-20 23:40:30 +0200

    tic tac toe program commit

--- testing.py
+++ testing.py
@@ -1,2 +1,7 @@
-list = (' ')*9
-print(list)
+def board_full_check(board_arg):
+    return not ' ' in board_arg         # return Class: boolean
+
+
+board_list = ['','1','2','3','4','5','6','7','8','9']
+
+print(board_full_check(board_list))
