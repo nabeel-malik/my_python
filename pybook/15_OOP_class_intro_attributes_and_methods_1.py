5e7248c4c2bd459bf8b8747144c49c18a674153a
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2019-05-31 15:29:01 +0200

    classes, attributes and methods commit

---
+++ pybook/15_OOP_class_intro_attributes_and_methods_1.py
@@ -0,0 +1,39 @@
+class Dog():
+    """
+    There are 2 kinds of attributes: Class object attributes, and user-defined attributes.
+    Class object attributes are common to each and every instance of the class. They are defined above '__init__'.
+    User-defined attributes are specific to each instance of the class. They are defined inside __init__'
+
+    '__init__' is a special method that is called whenever Python creates a new instance of the class (Instantiation).
+    The first parameter 'self' is mandatory, and just refers to that particular instance of the class.
+    By convention, the parameters passed to the '__init__' method have the same name as the user-defined attributes.
+    """
+
+    species = 'mammal'                  # 'mammal' assigned to CLASS OBJECT ATTRIBUTE called 'species'
+
+    def __init__(self, breed, name, spots):          # INSTANTIATION METHOD
+
+        # Expecting string
+        self.breed = breed              # breed parameter assigned to USER-DEFINED ATTRIBUTE called 'breed'
+        self.name = name
+
+        # Expecting boolean
+        self.spots = spots
+
+    def bark(self, mood):                     # CLASS METHOD called bark()
+        mood = mood.title()
+        print('{0} WOOF! from {1}'.format(mood, self.name))     # Notice how 'mood' parameter is not linked to 'self'
+
+
+my_dog = Dog('Huskie', 'Oscar', False)
+
+print('########## Printing instance type ##########')
+print(type(my_dog), '\n')
+
+print('########## Printing attributes ##########')
+print(my_dog.breed)
+print(my_dog.name)
+print(my_dog.spots, '\n')
+
+print('########## Running class method ##########')
+my_dog.bark('angry')            # Note: Attribute call has no () at the end, but method call does.
---
+++ pybook/16_OOP_class_intro_attributes_and_methods_2.py
@@ -0,0 +1,27 @@
+class Circle():
+
+    # CLASS OBJECT ATTRIBUTE
+    pi = 3.142
+
+    # INSTANTIATION METHOD
+    def __init__(self, radius=1):           # Can set default values for parameters just like regular functions.
+        self.radius = radius
+        self.area = Circle.pi * self.radius * self.radius       # We can create additional attributes inside '__init__'
+
+    # CLASS METHOD
+    def get_circumference(self):
+        return 2 * Circle.pi * self.radius       # We can use both 'Circle.pi' or 'self.pi'. 'Circle.pi' is preferred.
+
+
+my_circle = Circle(5)           # Defined a circle with radius 5. This is where 'area' attribute is calculated.
+
+print('########## Printing CLASS OBJECT ATTRIBUTE ##########')
+print(my_circle.pi,'\n')
+
+print('########## Printing USER-DEFINED ATTRIBUTES ##########')
+my_circle.radius = 5    # Reassigning a new value to the 'radius' attribute will NOT recalculate 'area' attribute.
+print(my_circle.radius)
+print(my_circle.area,'\n')
+
+print('########## Printing CLASS METHOD return ##########')
+print(my_circle.get_circumference())
