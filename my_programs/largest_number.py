866c0f0c334a78d2807694956deddc4cd5188552
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2020-01-03 09:08:19 +0100

    PEP 8 standard fixes

--- my_programs/largest_number.py
+++ my_programs/largest_number.py
@@ -6,7 +6,7 @@ num2 = float(list[1])
 num3 = float(list[2])
 
 if (num1 > num2) and (num1 > num3):
-    print ("{0} is the largest number".format(num1))
+    print("{0} is the largest number".format(num1))
 elif (num2 > num1) and (num2 > num3):
     print("{0} is the largest number".format(num2))
 else:
