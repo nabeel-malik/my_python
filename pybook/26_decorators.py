077bd7e5bc26cf7b5b57b8c0c4f4c1a4ab6e3ff0
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2020-01-16 13:18:30 +0100

    multiple decorators code and decorator classes

--- pybook/26_decorators.py
+++ pybook/26_decorators.py
@@ -46,7 +46,7 @@ def decorator_function(original_function):
     return wrapper_function
 
 
-@decorator_function         # this is the same as saying:   display = decorator_function(display)
+@decorator_function     # this is the same as: 'display = decorator_function(display)' placed AFTER display() definition
 def display():
     print('display() ran!')
 
@@ -73,7 +73,7 @@ def decorator_function(original_function):
     return wrapper_function
 
 
-@decorator_function         # this is the same as saying:   display = decorator_function(display)
+@decorator_function     # this is the same as: 'display = decorator_function(display)' placed AFTER display() definition
 def display():
     print('display() ran!')
 
@@ -124,9 +124,13 @@ print('\n############################## 5. LOGGING USE-CASE FOR DECORATOR FUNCTI
 The logging module is used to log how many times a particular function has been run, and with what arguments.
 To log any of the functions we use in our program, decorators alongwith the logging module can be used as an elegant
 solution.
+
+Notice how, in the following example, the decorator allows us to maintain our added functionality in one location, and
+apply it at multiple locations anywhere in our codebase.
 """
 
 
+# DECORATOR FUNCTION
 def my_logger(orig_func):
 
     import logging
@@ -145,3 +149,92 @@ def display_info(name, age):
 
 
 display_info('John', 25)
+display_info('Hans', 30)
+display_info('Layla', 33)
+
+
+print('\n############################## 6. TIMING USE-CASE FOR DECORATOR FUNCTIONS ##############################\n')
+"""
+DECORATORS can also be used to time how long it takes to run functions, without altering their original code.
+"""
+
+import time
+
+
+# DECORATOR FUNCTION
+def my_timer(orig_func):
+    def wrapper(*args, **kwargs):
+        t1 = time.perf_counter()
+        result = orig_func(*args, **kwargs)     # display_info() runs here
+        t2 = time.perf_counter()
+        print('{} ran in {} seconds'.format(orig_func.__name__, t2-t1))
+        return result           # this line can be commented out since orig_func(*args, **kwargs) has already been run
+
+    return wrapper
+
+
+@my_timer
+def display_info(name, age):
+    time.sleep(2)           # time delay added just to take 2 more seconds to run the function.
+    print('display_info() ran with arguments {} and {}!'.format(name, age))
+
+
+display_info('Layla', 33)
+
+
+print('\n#################### 7. COMBINED LOGGING AND TIMING USE-CASE FOR DECORATOR FUNCTIONS ####################\n')
+"""
+To implement 'both' DECORATOR FUNCTIONS on the display_info() function, we can simply add both @my_logger and @my_timer
+above display_info() function definition. This would be equivalent to display_info = my_logger(my_timer(display_info)).
+In this case, to ensure that, during execution, the functions are named correctly when running the decorators 
+sequentially, we have to import wraps from functools, and then decorate all of our wrapper functions with the 
+@wraps decorator, with the DECORATED FUNCTION argument name as an argument to it.
+"""
+
+from functools import wraps
+
+
+# LOGGING DECORATOR FUNCTION
+def my_logger(orig_func):
+
+    import logging
+    logging.basicConfig(filename='work_directory/decorators/{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    @wraps(orig_func)           # nested @wraps decorator, with the DECORATED FUNCTION argument name as an argument
+    def wrapper(*args, **kwargs):
+        logging.info('Ran with args: {}, and kwargs: {}'. format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+# TIMING DECORATOR FUNCTION
+def my_timer(orig_func):
+
+    @wraps(orig_func)           # nested @wraps decorator, with the DECORATED FUNCTION argument name as an argument
+    def wrapper(*args, **kwargs):
+        t1 = time.perf_counter()
+        result = orig_func(*args, **kwargs)     # display_info() runs here
+        t2 = time.perf_counter()
+        print('{} ran in {} seconds'.format(orig_func.__name__, t2-t1))
+        return result           # this line can be commented out since orig_func(*args, **kwargs) has already been run
+
+    return wrapper
+
+
+@my_logger
+@my_timer
+def display_info(name, age):
+    time.sleep(2)           # time delay added just to take 2 more seconds to run the function.
+    print('display_info() ran with arguments {} and {}!'.format(name, age))
+
+
+'''
+When we run the display_info() function now, we should expect to see all 3 of the following:
+    - display_info() executed with its contained print() message
+    - the time it took to run the function from the my_timer() DECORATOR FUNCTION, and
+    - a log of the function run in the display_info.log file created by the my_logger DECORATOR FUNCTION.
+and the time it took to run the function.
+'''
+display_info('Hemant', 48)
+
