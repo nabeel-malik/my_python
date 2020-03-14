63c493fb13360a2621fb654fd1a7aed38ba06d22
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2020-03-14 11:41:58 +0100

    edits to 17

--- pybook/17_OOP_inheritance.py
+++ pybook/17_OOP_inheritance.py
@@ -18,12 +18,12 @@ class Animal:                       # PARENT CLASS
         print('I am eating')
 
 
-class Dog(Animal):                  # CHILD CLASS inheriting from the BASE CLASS
+class Dog(Animal):                  # CHILD CLASS inheriting from the PARENT CLASS
     def __init__(self):
         Animal.__init__(self)       # Creating an instance of Animal() class when an instance of Dog() class is created.
         print('DOG CREATED')
 
-    def who_am_i(self):             # 'Over-writing' a method inherited from PARENT CLASS, aka METHOD OVERRIDING.
+    def who_am_i(self):             # 'Over-writing' a method inherited from PARENT CLASS, aka 'METHOD OVERRIDING'.
         print('I am a dog')
 
     def bark(self):                 # 'Adding' a method to the methods inherited from the PARENT CLASS
