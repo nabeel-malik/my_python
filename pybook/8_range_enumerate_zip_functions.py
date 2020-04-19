d0d0cba5904421b858a76e8bf87d565b747ea597
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2020-04-19 20:10:41 +0200

    added code for using zip() with unpacking operator

--- pybook/8_range_enumerate_zip_functions.py
+++ pybook/8_range_enumerate_zip_functions.py
@@ -3,8 +3,13 @@ The range() function RETURNS an immutable sequence of numbers between the given 
 
 The enumerate() function adds counter to an iterable and RETURNS an enumerate object (in our case a list of tuples).
 
-The zip() function takes iterables (can be zero or more), makes an iterator that aggregates elements
-based on the iterables passed, and RETURNS an iterator of tuples.
+The zip() function takes in iterables as arguments and returns an iterator of tuples. This iterator generates a
+series of tuples containing elements from each iterable. zip() can accept any type of iterable, such as files, lists,
+tuples, dictionaries, sets, and so on.
+
+The zip() function can also be used to UNZIP using the UNPACKING OPERATOR (*). Say you have a list of tuples and want
+to separate the elements of each tuple into independent sequences. To do this, you can use zip() along with the
+unpacking operator (*). See section 4.
 """
 
 print('\n#################### 1. range() ####################\n')
@@ -42,7 +47,7 @@ print('for index, item in enumerate(foodlist, start=1):\t\tprint(index, item)')
 for index, item in enumerate(foodlist, start=1):    # unpacking the enumerate object
     print(index, item)
 
-print('\n#################### 3. zip() ####################\n')
+print('\n#################### 3. USING zip() TO ZIP ITERABLES ####################\n')
 
 mylist1 = ['a','b','c']
 mylist2 = [1,2,3]
@@ -68,5 +73,21 @@ print('for item1, item2 in zip(mylist1, mylist2):\t\tprint(item1, item2)')
 for item1, item2 in zip(mylist1, mylist2):      # unpacking the returned iterator of tuples
     print(item1, item2)
 
+print('\n#################### 4. USING zip() TO UNZIP WITH UNPACKING OPERATOR (*) ####################\n')
+'''
+Say you have a LIST OF TUPLES and want to separate the elements of each tuple into INDEPENDENT SEQUENCES. 
+To do this, you can use zip() along with the UNPACKING OPERATOR *, like so:
+'''
 
+pairs = [(1, 'a'), (2, 'b'), (3, 'c'), (4, 'd')]
 
+numbers_tuple, letters_tuple = zip(*pairs)                  # will create 2 independent tuples
+numbers_list, letters_list = map(list, zip(*pairs))         # will create 2 independent lists
+
+print("Two independent tuples:")
+print(numbers_tuple)
+print(letters_tuple)
+print()
+print("Two independent lists:")
+print(numbers_list)
+print(letters_list)
