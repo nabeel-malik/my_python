da180191c4c141a2a6ff8cfb771b8131c70b3c30
Nabeel Mohammad Malik <nabeel.m.malik@gmail.com>
2020-09-17 09:49:08 +0200

    my_programs/test.py edits

--- my_programs/test.py
+++ my_programs/test.py
@@ -1,25 +1,10 @@
-def _remove_comments(clear_comments, tmp_in):
-    """
-    Remove comments, when needed, in the tmp_in string.
-    In-line comments will not be removed.
+import os
 
-    :param clear_comments: Boolean describing whether to remove comments.
-    :param tmp_in: String of text to remove Eclipse comments.
-    :return: tmp_in or tmp_in without comments depending on clear_comments
-    """
-    tmp_out = tmp_in
-    if clear_comments:
-        if "--" in tmp_out:
-            tmp_out = "%s\n" % tmp_out.split("--")[0]
-    return tmp_out
+dir1 = 'G:/TPD/FC/CRM/RNB/RNB2021/RNB-files/Files from Sharepoint/10_sep'
+dir2 = 'G:/TPD/FC/CRM/RNB/RNB2021/RNB-files/Files from Sharepoint/10 sep'
 
+set1 = set(os.listdir(dir1))
+set2 = set(os.listdir(dir2))
 
-str1 = '-- 2 regions '
-str2 = "'include/CASE_70_PROP_FIPNUM.GRDECL' / "
-str3 = "INCLUDE                                -- Generated : Petrel "
-str4 = "'include/CASE_70_PROP_FIPNUM.GRDECL' / -- dummy "
-
-print(_remove_comments(True, str1))
-print(_remove_comments(True, str2))
-print(_remove_comments(True, str3))
-print(_remove_comments(True, str4))
+for item in set1.difference(set2):
+    print(item)
